830a6f2a6d962f23239bc726e3a5c66864e498b5
zapshooter <92568875+zapshooter@users.noreply.github.com>
2026-08-02 23:40:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function shortcut(s1, s2) {
-  // your code here
+  if (s1 === "" || s2 === "") {
+    return "";
+  }
+  return s1[0] + s2[0];
 }
-
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
